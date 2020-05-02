b0db1b57e2892c41d95bd3ab1c97cdf8f06deaf7
PARKJUHONG123 <corallines@naver.com>
2020-05-02 22:42:08 +0900

    add maximum distance compare function to make simple points on the line (problem condition added)

--- 6_1_Geometric_Outside/Geometry_Problem/src/solution.java
+++ 6_1_Geometric_Outside/Geometry_Problem/src/solution.java
@@ -102,6 +102,8 @@ public class solution {
     }
     
     
+    // min_version (every point on the line)
+    /* 
     static int PointWrapping (Point P[], int n) {
     	int i, NumVertex;
     	float MinAngle, MaxAngle, Angle;
@@ -135,7 +137,6 @@ public class solution {
 			
 			float min_distance = Float.MAX_VALUE;
 			float distance = Float.MAX_VALUE;
-			
 			for (i = NumVertex + 1; i <= n; i++) {
 				if (P[i].get_angle() == MaxAngle) {
 					distance = (P[NumVertex].get_X() - P[i].get_X()) * (P[NumVertex].get_X() - P[i].get_X()) 
@@ -146,7 +147,56 @@ public class solution {
 					}
 				}
 			}
+		} while (NextPoint != n);
+		return ++NumVertex;
+    }
+    */
+    
+
+    // max_version (maximum point on the line)
+    static int PointWrapping (Point P[], int n) {
+    	int i, NumVertex;
+    	float MinAngle, MaxAngle, Angle;
+    	int FirstPoint = 0, NextPoint = -1;
+    	    	
+    	for (i = 1; i < n; i++) {
+    		if (P[i].get_Y() < P[FirstPoint].get_Y()) {
+    			FirstPoint = i;
+    		}
+    	}
+    	NumVertex = -1;
+		P[n] = P[FirstPoint];
+		MaxAngle = (float) 0.0;
+		NextPoint = FirstPoint;
+		
+		do {
+			NumVertex++;
+			swap(P[NumVertex], P[NextPoint]);
+			NextPoint = n;
+			MinAngle = MaxAngle;
+			MaxAngle = (float) 360.0;
+			for (i = NumVertex + 1; i <= n; i++) {
+				Angle = ComputeAngle(P[NumVertex], P[i]);
+				P[i].set_angle(Angle);
+				if (Angle >= MinAngle && Angle < MaxAngle ) {
+					if (P[NumVertex].get_X() != P[i].get_X() || P[NumVertex].get_Y() != P[i].get_Y()) {
+						MaxAngle = Angle;						
+					}
+				}
+			}
 			
+			float max_distance = Float.MIN_VALUE;
+			float distance = Float.MIN_VALUE;
+			for (i = NumVertex + 1; i <= n; i++) {
+				if (P[i].get_angle() == MaxAngle) {
+					distance = (P[NumVertex].get_X() - P[i].get_X()) * (P[NumVertex].get_X() - P[i].get_X()) 
+							+ (P[NumVertex].get_Y() - P[i].get_Y()) * (P[NumVertex].get_Y() - P[i].get_Y());
+					if (max_distance < distance && distance > 0) {
+						max_distance = distance;
+						NextPoint = i;
+					}
+				}
+			}
 		} while (NextPoint != n);
 		return ++NumVertex;
     }
