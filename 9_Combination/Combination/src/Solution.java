ba4d707b7fb67041a13dddf8610fe3cd58dc8ea9
JUHONG PARK <corallines@naver.com>
2020-05-29 16:05:18 +0900

    change int to bigInteger

--- 9_Combination/Combination/src/Solution.java
+++ 9_Combination/Combination/src/Solution.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.math.BigInteger;
 
 public class Solution {
 	public static void main(String[] args) {
@@ -9,17 +10,17 @@ public class Solution {
 		int i = 0;
 		
 		scanner.close();
-		int[] d_arr = new int[k];
+		BigInteger[] d_arr = new BigInteger[k];
 		
 		for (i = 0; i < k; i++) {
-			d_arr[i] = 0;
+			d_arr[i] = new BigInteger(String.valueOf(0));
 		}
 		for (i = 0; i < k; i++) {
 			if (i == 0) {
-				d_arr[i] = n;
+				d_arr[i] = new BigInteger(String.valueOf(n));
 			}
 			else {
-				d_arr[i] = d_arr[i - 1] * (n - i) / (i + 1);
+				d_arr[i] = d_arr[i - 1].multiply(new BigInteger(String.valueOf(n - i))).divide(new BigInteger(String.valueOf(i + 1)));
 			}
 		}
 		System.out.println(d_arr[k - 1]);
